6c1e19c484f3abf28e5b3be7598de237f661b03b
Mitchell Paulus <paulusm14@gmail.com>
2019-10-01 12:32:57 -0500

    Check for pacman before running ruby command

--- scripts/install-website.sh
+++ scripts/install-website.sh
@@ -6,9 +6,15 @@ if [ ! -d ~/mitchpaulus.github.io ]; then
     git clone https://github.com/mitchpaulus/mitchpaulus.github.io.git ~/mitchpaulus.github.io
 fi
 
-if ! ( pacman -Qi ruby >/dev/null 2>/dev/null ); then
-    printf "Need to install ruby...\n"
-    sudo pacman -S ruby
+
+if command -v pacman >/dev/null; then
+    if ! ( pacman -Qi ruby >/dev/null 2>/dev/null ); then
+        printf "Need to install ruby...\n"
+        sudo pacman -S ruby
+    fi
+
 fi
 
 gem install bundler jekyll
+
+
