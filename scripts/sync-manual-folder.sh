6b0235fcd2de414f82400d33bb2e71585e75b2b4
Mitchell Paulus <paulusm14@gmail.com>
2019-10-30 08:04:10 -0500

    Add script to bootstrap new manual folder in FolderSync

---
+++ scripts/sync-manual-folder.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Purpose of this script is to bootstrap the generation of folders for FolderSync
+mkdir Manual && cd Manual && mkdir FailedToUpload && mkdir Logs && mkdir NeedToPreProcess && mkdir NeedToUpload && mkdir Uploaded
