acca2ddb2bd2a53661865fd42e28ce2558d76921
Mitchell Paulus <paulusm14@gmail.com>
2020-03-04 14:45:00 -0600

    Add Excel macros

---
+++ excel_macros.vb
@@ -0,0 +1,21 @@
+Sub CommandHeader()
+'
+' CommandHeader Macro
+
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .Color = 8866048
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+
+    Selection.Font.Color = RGB(255, 255, 255)
+    Selection.Font.Bold = True
+End Sub
+
+
+Sub FormatThousands()
+    Selection.NumberFormat = "#,##0"
+End Sub
+
