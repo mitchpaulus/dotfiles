dc0cd3c49e8b4a3a58a88644b4fe9740bf647339
Mitchell Paulus <paulusm14@gmail.com>
2020-07-24 08:53:03 -0500

    Fix up tmux attach script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -66,5 +66,18 @@ function compass
 end
 
 
+function ta
+    set -l IFS
+    switch (tmux ls 2>&1)
+        case "no server running*"
+            printf "No tmux server currently running\n"
+        case '*'
+            if test (tmux ls | wc -l) -gt 1
+                tmux attach-session -t (tmux ls | awk -F ':' '{print $1}' | fzf)
+            else
+                tmux attach-session -t (tmux ls | awk -F ':' '{print $1;exit}')
+            end
+    end
+end
 
 # vim:ft=fish
