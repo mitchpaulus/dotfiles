355bca74ae73ac9d19df48980167396e13312d94
Mitchell Paulus <paulusm14@gmail.com>
2020-09-06 11:42:38 -0500

    Add help to linear_reset_excel.sh function

--- scripts/linear_reset_excel.sh
+++ scripts/linear_reset_excel.sh
@@ -9,4 +9,14 @@
 # $4 = X2
 # $5 = Y2
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    cat <<EOF
+linear_reset_excel.sh
+
+USAGE:
+    linear_reset_excel.sh Input X1 Y1 X2 Y2
+EOF
+exit 0
+fi
+
 printf "=IF($1 < $2, $3, IF($1 > $4, $5, $3 + (($5 - $3)/($4 - $2)*($1 - $2))))\n"
