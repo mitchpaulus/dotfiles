5c1252e6be5549dde3d66cf5d0e06355dba5fc87
Mitchell Paulus <paulusm14@gmail.com>
2021-01-11 13:53:34 -0600

    Add help to FolderSync folder creation script

--- scripts/foldersync_folder_creation.sh
+++ scripts/foldersync_folder_creation.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
+
+help() {
+    cat <<EOF
+foldersync_folder_creation.sh
+
+Creates the following folders for a given BAS:
+1. FailedToUpload
+2. Logs
+3. NeedToPreProcess
+4. NeedToUpload
+5. Uploaded
+EOF
+}
+
+if test "$1" = "-h" || test "$1" = "--help"; then
+    help
+    exit 0
+fi
+
 mkdir -p FailedToUpload && mkdir -p Logs && mkdir -p NeedToPreProcess && mkdir -p NeedToUpload && mkdir -p Uploaded
