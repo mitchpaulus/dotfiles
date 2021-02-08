c83a05cd94a9fb76a3eab72910450f0d85e6eeb3
Mitchell Paulus <paulusm14@gmail.com>
2021-02-07 22:10:01 -0600

    Update 'eh' script to work on Arch as well

--- .config/fish/functions/eh.fish
+++ .config/fish/functions/eh.fish
@@ -1,3 +1,9 @@
 function eh
-    explorer.exe (wslpath -w (pwd))
+    if command -v explorer.exe >/dev/null 2>/dev/null
+        explorer.exe (wslpath -w (pwd))
+    else if command -v dolphin >/dev/null 2>/dev/null
+        dolphin . &
+    else
+        printf "No GUI file manager set up.\n"
+    end
 end
