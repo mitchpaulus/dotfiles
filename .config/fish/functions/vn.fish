6e2ee475025cdf7d05dc55cffc69dbe2ef58fa81
Mitchell Paulus <paulusm14@gmail.com>
2021-11-12 12:46:19 -0600

    Update 'view note' function to work on WSL

--- .config/fish/functions/vn.fish
+++ .config/fish/functions/vn.fish
@@ -1,16 +1,21 @@
 # View Note
 
 function vn --description="view note"
-
     set note (select_note)
 
-    if test "$status" -eq 0
-        set pdfname "$MPNOTES/$note.pdf"
-        redo-ifchange "$pdfname"
+    if test "$status" -ne 0
+        return $status
     end
 
+    set pdfname "$MPNOTES/$note.pdf"
+    redo-ifchange "$pdfname"
+
     if test -f "$pdfname"
-        xdg-open "$pdfname" > /dev/null 2> /dev/null
+        if  test -n "$WSL_DISTRO_NAME"; and command -q wsl-opener
+            wsl-opener "$pdfname"
+        else
+            xdg-open "$pdfname" > /dev/null 2> /dev/null
+        end
         # pandoc --pdf-engine=lualatex -o "$pdfname" "$MPNOTES/$note.md"
     end
 end
