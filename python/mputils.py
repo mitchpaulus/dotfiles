5f12b7b50775e11377a338c53673f7d3cb1f6796
Mitchell Paulus <paulusm14@gmail.com>
2021-12-01 16:09:42 -0600

    Add version/natural sort to python utilities

--- python/mputils.py
+++ python/mputils.py
@@ -45,3 +45,26 @@ def read_tsv(file_path: str) -> List[List[str]]:
     """
     with open(file_path, encoding="utf-8") as file:
         return [line.split('\t') for line in file.read().splitlines()]
+
+
+def convert_to_int_if_possible(s: str) -> Union[int, str]:
+    """
+    Convert the given string to an int if possible. Otherwise, return the
+    original string.
+    """
+    return int(s) if s.isdigit() else s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [convert_to_int_if_possible(c) for c in re.split('([0-9]+)', s)]
+
+# Basically taken from https://stackoverflow.com/a/2669120/5932184
+def version_sort(l: Iterable[str]) -> List[str]:
+    """ Sort the given iterable in the way that humans expect."""
+    return sorted(l, key=alphanum_key)
+
+def version_sort_in_place(l: List[str]):
+    """ Sort the given list in the way that humans expect."""
+    l.sort(key=alphanum_key)
