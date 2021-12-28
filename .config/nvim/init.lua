d25ed6d7e3d3999f96ef70c67f99226896b3dcfb
Mitchell Paulus <paulusm14@gmail.com>
2021-12-28 16:42:57 -0600

    Change <C-space> mapping for completion
    I often am trying to space when having the completion window open, and I
    want to enter a space even if I hold onto the control key too long.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,7 +71,7 @@ if vim.fn.has('nvim-0.5.0') == 1 then
     mapping = {
       ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-s>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
       ['<C-e>'] = cmp.mapping({
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
