658d8c6754f0d3db4d7159cf6e2b3b1936a9d080
Mitchell Paulus <paulusm14@gmail.com>
2022-01-10 17:00:23 -0600

    Re-add bat highlighting for lf

--- scripts/bat-lf-previewer.sh
+++ scripts/bat-lf-previewer.sh
@@ -4,5 +4,15 @@
 # https://github.com/gokcehan/lf/issues/234
 # https://github.com/sharkdp/bat/issues/490#issuecomment-511338924
 
+# $1 = file path
+# $2 = optional number of lines to show
+
 unset COLORTERM
-bat --color always --style 'plain' --theme 'ansi-dark' "$1"
+
+# if two arguments
+if $# -eq 2; then
+    bat --italic-text always --color always --style 'plain' --theme 'ansi-dark' --line-range :"$2" "$1"
+else
+    bat --italic-text always --color always --style 'plain' --theme 'ansi-dark' "$1"
+fi
+
