d4d8d9722d5d632759609b429703f0d25067b9c2
Mitchell Paulus <paulusm14@gmail.com>
2022-05-12 13:14:47 -0500

    Add tabula directory on Windows machine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,6 +59,7 @@ path_prepend "$HOME/.gem/ruby/2.7.0/bin"
 path_prepend "$HOME/.gem/ruby/3.0.0/bin"
 path_prepend "$HOME/bin"
 path_prepend "$HOME/.local/bin"
+path_prepend "/mnt/d/PortablePrograms/tabula-win-1.2.1/tabula"
 
 # Neovim/Vim for all the things
 if command -v nvim >/dev/null 2>&1
