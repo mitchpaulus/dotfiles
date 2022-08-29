2abf30e43a399bcdd90f8e117a3a4b9d06170033
Mitchell Paulus <paulusm14@gmail.com>
2022-08-29 07:02:59 -0500

    Fix cmp.mapping.complete() binding
    CTRL-L was already a favorite mapping of mine. CTRL-; shouldn't have any
    conflicts.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -85,7 +85,7 @@ cmp.setup({
     mapping = {
         -- ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         -- ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-L>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-;>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
         ['<C-e>'] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
