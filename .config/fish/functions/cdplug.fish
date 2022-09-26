aa022e9fbad8ad78481dafe7be63c9f1d29d90b5
Mitchell Paulus <paulusm14@gmail.com>
2022-09-26 06:54:02 -0500

    Fix cdplug fish function

--- .config/fish/functions/cdplug.fish
+++ .config/fish/functions/cdplug.fish
@@ -1,8 +1,10 @@
 function cdplug --description 'Quickly change to Neovim plugin dir'
     if test -d $HOME"/.local/share/nvim/plugged"
         cd $HOME"/.local/share/nvim/plugged"
+    else if test -d $HOME/.config/nvim/plugged
+        cd $HOME/.config/nvim/plugged
     else
         printf 'Could not find dir. Add to cdplug.fish function script\n' >&2
-        exit 1
+        false
     end
 end
