5aac9a4181c5e9ad226a02698e2456031216c965
Mitchell Paulus <paulusm14@gmail.com>
2022-10-13 13:10:11 -0500

    Also search for WT preview dir

--- .config/fish/functions/cdwt.fish
+++ .config/fish/functions/cdwt.fish
@@ -4,5 +4,16 @@ function cdwt
 
     set WT_DIR $LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
 
-    cd $WT_DIR
+    if test -d $WT_DIR
+        cd $WT_DIR
+    else
+        set WT_DIR $LOCALAPPDATA/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/
+
+        if test -d $WT_DIR
+            cd $WT_DIR
+        else
+            printf 'Could not find Windows Terminal directory.\n' >&2
+        end
+    end
+
 end
