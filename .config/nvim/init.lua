a3b7b2eb32f6dc1a3a719e459f0981ff2a47de19
Mitchell Paulus <paulusm14@gmail.com>
2022-12-20 15:48:24 -0600

    Always show autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,7 +71,6 @@ end
 local cmp = require'cmp'
 
 cmp.setup({
-    completion = { keyword_length = 1 },
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
