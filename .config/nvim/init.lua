321a5b684c57e9e2c35b8f3c9c3ed4ec6c95fd3c
Mitchell Paulus <paulusm14@gmail.com>
2023-05-09 17:05:21 -0500

    Try out some telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -655,6 +655,39 @@ vim.g.copilot_filetypes = {
     markdown = true,
 }
 
+
+-- Trying out custom telescope picker
+local pickers      = require('telescope.pickers')
+local finders      = require('telescope.finders')
+local conf         = require('telescope.config').values
+local actions      = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+-- our picker function: colors
+tele_colors = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "colors",
+    finder = finders.new_table {
+      results = { "red", "green", "blue" }
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        -- print(vim.inspect(selection))
+        vim.api.nvim_put({ selection[1] }, "", false, true)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+-- -- to execute the function
+-- colors()
+
+
 -- vim-vsnip {{{
 -- https://github.com/hrsh7th/vim-vsnip
 -- Taken nearly directly out of the README.
