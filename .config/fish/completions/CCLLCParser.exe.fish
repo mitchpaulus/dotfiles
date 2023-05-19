97069238b09548dacae5354ef70af94b9184a701
Mitchell Paulus <paulusm14@gmail.com>
2023-05-19 14:41:10 -0500

    Add fish completion

--- .config/fish/completions/CCLLCParser.exe.fish
+++ .config/fish/completions/CCLLCParser.exe.fish
@@ -1,4 +1,4 @@
-complete -c CCLLCParser.exe -s p -x -a "ccllc fid_abq_bas fid_wlk icon metasys_export_tool meterlogic simple_csv"
+complete -c CCLLCParser.exe -s p -x -a "ccllc fid_abq_bas fid_wlk fid_wlk_html icon metasys_export_tool meterlogic simple_csv"
 complete -c CCLLCParser.exe -s t -l trends -d 'Print summary of available trends rather than parse'
 complete -c CCLLCParser.exe -l csv -d 'Print to CSV format instead of CCLLC'
 complete -c CCLLCParser.exe -s c -l count -d 'Also show data point count with trend summary'
