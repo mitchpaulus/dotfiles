3074eb6b635fae7d13077f92454273b6fcd42759
Mitchell Paulus <paulusm14@gmail.com>
2023-09-04 20:21:44 -0500

    Add macOS location for nvim

--- .config/fish/functions/gd.fish
+++ .config/fish/functions/gd.fish
@@ -1,1 +1,9 @@
-function gd; cd ~/dotfiles; end
+function gd;
+    if test -d $HOME/dotfiles
+        cd $HOME/dotfiles
+    else if test -d $REPOS/dotfiles
+        cd $REPOS/dotfiles
+    else
+        printf 'Could not find dotfiles directory\n'
+    end
+end
