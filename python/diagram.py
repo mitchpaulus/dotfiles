8a175a7fa8243f0bb886643042888424f95de5df
Mitchell Paulus <paulusm14@gmail.com>
2024-03-15 16:56:48 -0500

    Improvements to diagram.py

--- python/diagram.py
+++ python/diagram.py
@@ -1,13 +1,72 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Union, Any
+import uuid
+
+# General iterface convention:
+# - Shapes should have a to_vba() and to_svg() method
+# - Shapes should have a min_x and min_y method to get the minimum x and y coordinate
 
 # Type alias for float | Callable[[], float]
 Coord = float | Callable[[], float]
 
-def compute(x: Coord) -> float:
+def compute(x):
     if callable(x):
         return x()
     else:
         return x
+
+
+class Drawing:
+    def __init__(self) -> None:
+        self._shapes = {}
+
+    def rect(self, name: Optional[str] = None) -> 'Rectangle':
+        if name is None:
+            name = f"rect{len(self._shapes)}"
+
+        if name in self._shapes:
+            raise KeyError(f"Shape with name {name} already exists")
+
+        self._shapes[name] = Rectangle(name)
+        return self._shapes[name]
+
+    def line(self, name: Optional[str] = None) -> 'Line':
+        if name is None:
+            name = f"line{len(self._shapes)}"
+
+        if name in self._shapes:
+            raise KeyError(f"Shape with name {name} already exists")
+
+        self._shapes[name] = Line(name)
+        return self._shapes[name]
+
+    def text(self, name: Optional[str] = None) -> 'Text':
+        if name is None:
+            name = f"text{len(self._shapes)}"
+
+        if name in self._shapes:
+            raise KeyError(f"Shape with name {name} already exists")
+
+        self._shapes[name] = Text(name)
+        return self._shapes[name]
+
+    def circle(self, name: Optional[str] = None) -> 'Circle':
+        if name is None:
+            name = f"circle{len(self._shapes)}"
+
+        if name in self._shapes:
+            raise KeyError(f"Shape with name {name} already exists")
+
+        self._shapes[name] = Circle(name)
+        return self._shapes[name]
+
+
+    def to_svg(self, min_x: float, min_y: float, width: float, height: float):
+        print(f'<svg viewBox="{min_x} {min_y} {width} {height}" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">\n', end='')
+        for i in self._shapes:
+            shape = self._shapes[i]
+            print(shape.to_svg(), end="")
+        print('</svg>')
+
 
 # Use fluent syntax
 class Rectangle:
@@ -17,15 +76,31 @@ class Rectangle:
         self._width: Coord = 1
         self._height: Coord = 1
         self._name = name
+        self._text = None
+        self._font_size = None
 
         self._fill: Optional[tuple[int, int, int]] = None
 
-    def x(self, x: Coord) -> 'Rectangle':
+    def x(self, x = None) -> Any:
+        if x is None:
+            return self._x
+
         self._x = x
         return self
 
-    def y(self, y: Coord) -> 'Rectangle':
+    def y(self, y = None) -> Any:
+        if y is None:
+            return self._y
         self._y = y
+        return self
+
+    def text(self, text: str) -> 'Rectangle':
+        """This is a text element that is tied to the center of the rectangle."""
+        self._text = Text(uuid.uuid4().hex).text(text).x(self.center_x()).y(self.center_y()).xAnchor("middle").yAnchor("middle")
+        return self
+
+    def font_size(self, font_size: float) -> 'Rectangle':
+        self._font_size = font_size
         return self
 
     def x2(self) -> Coord:
@@ -34,11 +109,11 @@ class Rectangle:
     def y2(self) -> Coord:
         return lambda: compute(self._y) + compute(self._height)
 
-    def center_x(self) -> float:
-        return compute(self._x) + compute(self._width) / 2
+    def center_x(self):
+        return lambda: compute(self._x) + compute(self._width) / 2
 
-    def center_y(self) -> float:
-        return compute(self._y) + compute(self._height) / 2
+    def center_y(self):
+        return lambda:compute(self._y) + compute(self._height) / 2
 
     def width(self, width: Coord) -> 'Rectangle':
         self._width = width
@@ -80,6 +155,7 @@ class Rectangle:
 
     def to_svg(self):
         x = compute(self._x)
+        # The y-origin is at the top in SVG instead of bottom, so we need to subtract the height
         y = -compute(self._y) - compute(self._height)
         width = compute(self._width)
         height = compute(self._height)
@@ -90,6 +166,12 @@ class Rectangle:
             fill_str = 'fill="none"'
 
         rect_tag = f'<rect stroke="black" stroke-width="0.1" x="{x}" y="{y}" width="{width}" height="{height}" {fill_str} />\n'
+
+        if self._text:
+            if self._font_size:
+                self._text.font_size(self._font_size)
+            rect_tag += self._text.to_svg()
+
         return rect_tag
 
     def right_of(self, other, distance = 72) -> 'Rectangle':
@@ -116,11 +198,12 @@ class Rectangle:
         return f'Rectangle({self.width}, {self.height})'
 
 class Line:
-    def __init__(self):
+    def __init__(self, name):
         self._x1: Coord = 0.0
         self._y1: Coord = 0.0
         self._x2: Coord = 1
         self._y2: Coord = 1
+        self._name = name
 
     def x1(self, x1: Coord) -> 'Line':
         self._x1 = x1
@@ -158,6 +241,7 @@ class Text:
         self._font_size = None
         self._xAnchor = "start"
         self._yAnchor = "auto"
+        self._font_family = "sans-serif"
 
     def x(self, x: Coord) -> 'Text':
         self._x = x
@@ -167,7 +251,10 @@ class Text:
         self._y = y
         return self
 
-    def text(self, text: str) -> 'Text':
+    def text(self, text = None) -> Any:
+        if text is None:
+            return self._text
+
         self._text = text
         return self
 
@@ -193,7 +280,37 @@ class Text:
         font_size = self._font_size or 12
         xAnchor = self._xAnchor
         dominant_baseline = self._yAnchor
-        return f'<text x="{x}" y="{y}" font-size="{font_size}" text-anchor="{xAnchor}" dominant-baseline="{dominant_baseline}">{self._text}</text>\n'
+        return f'<text font-family="{self._font_family}" x="{x}" y="{y}" font-size="{font_size}" text-anchor="{xAnchor}" dominant-baseline="{dominant_baseline}">{self._text}</text>\n'
+
+
+class Circle:
+    def __init__(self, name) -> None:
+        self._x = 0
+        self._y = 0
+        self._r = 10
+        self._name = name
+
+    def x(self, x: Coord) -> 'Circle':
+        self._x = x
+        return self
+
+    def y(self, y: Coord) -> 'Circle':
+        self._y = y
+        return self
+
+    def r(self, r: Coord) -> 'Circle':
+        self._r = r
+        return self
+
+    def to_vba(self):
+        vba = f"ActiveSheet.Shapes.AddShape(msoShapeOval, {compute(self._x)}, {compute(self._y)}, {compute(self._r)}, {compute(self._r)})\n"
+        return vba
+
+    def to_svg(self):
+        x = compute(self._x)
+        y = -compute(self._y)
+        r = compute(self._r)
+        return f'<circle cx="{x}" cy="{y}" r="{r}" />\n'
 
 
 class Sensor:
