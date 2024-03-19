82d8d18187e0775696eedc379909f5311dac2cb1
Mitchell Paulus <paulusm14@gmail.com>
2024-03-19 09:42:13 -0500

    Move neobem-vim plugin out of vim-plug

---
+++ vim/install_plugins_nvim.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+mkdir -p ~/.config/nvim/pack/mp/start/
+
+while read -r plugin; do
+    if test -d ~/.config/nvim/pack/mp/start/"$plugin"; then
+        echo "Plugin $plugin already installed"
+        continue
+    fi
+    git clone git@github.com:mitchpaulus/"$plugin".git
+done < plugins.txt
