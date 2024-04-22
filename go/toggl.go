f61dff16876e265df463a3f579c2d03fa973c7b0
Mitchell Paulus <paulusm14@gmail.com>
2024-04-22 14:18:00 -0500

    Add functionality to toggl utility

--- go/toggl.go
+++ go/toggl.go
@@ -9,6 +9,7 @@ import (
     "encoding/json"
     "math"
     "strings"
+    "bytes"
 )
 
 
@@ -30,6 +31,54 @@ type TogglTimeEntry struct {
     UserID            int64      `json:"user_id"`
     UID               int64      `json:"uid"`
     WID               int64      `json:"wid"`
+}
+
+type TogglWorkspace struct {
+    Name string `json:"name"`
+    Id int64 `json:"id"`
+}
+
+type TogglProject struct {
+    ID int64 `json:"id"`
+    Name string `json:"name"`
+    WorkspaceId int64 `json:"workspace_id"`
+}
+
+type TogglProjectPost struct {
+
+// name	type	description
+// billable	boolean	Whether the time entry is marked as billable, optional, default false
+// created_with	string	Must be provided when creating a time entry and should identify the service/application used to create it
+// description	string	Time entry description, optional
+// duration	integer	Time entry duration. For running entries should be negative, preferable -1
+// duronly	boolean	Deprecated: Used to create a time entry with a duration but without a stop time. This parameter can be ignored.
+// pid	integer	Project ID, legacy field
+// project_id	integer	Project ID, optional
+// shared_with_user_ids	Array of integer	List of user IDs to share this time entry with
+// start	string	Start time in UTC, required for creation. Format: 2006-01-02T15:04:05Z
+// start_date	string	If provided during creation, the date part will take precedence over the date part of "start". Format: 2006-11-07
+// stop	string	Stop time in UTC, can be omitted if it's still running or created with "duration". If "stop" and "duration" are provided, values must be consistent (start + duration == stop)
+// tag_action	string	Can be "add" or "delete". Used when updating an existing time entry
+// tag_ids	Array of integer	IDs of tags to add/remove
+// tags	Array of string	Names of tags to add/remove. If name does not exist as tag, one will be created automatically
+// task_id	integer	Task ID, optional
+// tid	integer	Task ID, legacy field
+// uid	integer	Time Entry creator ID, legacy field
+// user_id	integer	Time Entry creator ID, if omitted will use the requester user ID
+// wid	integer	Workspace ID, legacy field
+// workspace_id	integer	Workspace ID, required
+
+    CreatedWith string `json:"created_with"`
+    Description string `json:"description"`
+    Duration int64 `json:"duration"`
+    ProjectId int64 `json:"project_id"`
+    Start string `json:"start"`
+    WorkspaceId int64 `json:"workspace_id"`
+
+
+
+
+
 }
 
 func main() {
@@ -61,6 +110,11 @@ func main() {
             os.Exit(0)
         } else if os.Args[i] == "ts" {
             command = "ts"
+        } else if os.Args[i] == "ws" {
+            command = "ws"
+        } else if (os.Args[i] == "lunch") {
+            // Start lunch timer
+            command = "lunch"
         } else {
             // Print to stderr
             fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[i])
@@ -72,6 +126,99 @@ func main() {
     }
 
     toggl_err := `Toggl API error`
+
+    if command == "ws" {
+        // Get the workspaces for currrent user
+        // https://engineering.toggl.com/docs/api/me#get-workspaces
+
+        workspaces, err := getWorkspaces(token)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+
+        // Print the workspaces to stdout
+        for _, ws := range workspaces {
+            fmt.Printf("%d\t%s\n", ws.Id, ws.Name)
+        }
+
+        os.Exit(0)
+    } else if command == "lunch" {
+
+        // Look for project with name "Lunch", case-insensitive
+        projects, err := get_projects(token)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+
+        // Check if project with name "Lunch" exists
+        var lunch_id int64
+        var lunchProject TogglProject
+        for _, project := range projects {
+            if strings.ToLower(project.Name) == "lunch" {
+                lunch_id = project.ID
+                lunchProject = project
+                break
+            }
+        }
+
+        if lunch_id == 0 {
+            fmt.Fprintf(os.Stderr, "Lunch project not found\n")
+            os.Exit(1)
+        }
+
+        // Print lunch Id to STDERR
+        fmt.Fprintf(os.Stderr, "Lunch project ID: %d\n", lunch_id)
+
+        workspaceId := lunchProject.WorkspaceId
+
+        // Start timer for lunch project
+        // https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/time_entries
+        TogglProjectPost := TogglProjectPost{
+            CreatedWith: "toggl_cli",
+            Description: "Lunch",
+            Duration: -1,
+            ProjectId: lunch_id,
+            Start: time.Now().UTC().Format("2006-01-02T15:04:05Z"),
+            WorkspaceId: workspaceId,
+        }
+
+        postBytes, err := json.Marshal(TogglProjectPost)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error in marshalling TogglProjectPost\n")
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+
+        req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.track.toggl.com/api/v9/workspaces/%d/time_entries", workspaceId), bytes.NewBuffer(postBytes))
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error in building new request.\n")
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+
+        req.Header.Set("Content-Type", "application/json; charset=utf-8")
+        req.SetBasicAuth(token, "api_token")
+
+        client := &http.Client{}
+        resp, err := client.Do(req)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error in POST request for lunch\n")
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+
+        // Check the response code
+        if resp.StatusCode != 200 {
+            fmt.Fprintf(os.Stderr, "Error in POST request for lunch\n")
+            fmt.Fprintf(os.Stderr, toggl_err)
+            os.Exit(1)
+        }
+        os.Exit(0)
+    }
+
+    // Else command is either "current" or "stop", need to get the current time entry for both
     req, err := http.NewRequest(http.MethodGet, "https://api.track.toggl.com/api/v9/me/time_entries/current", nil)
     if err != nil {
         fmt.Fprintf(os.Stderr, "Error in building new request.\n")
@@ -225,10 +372,17 @@ func main() {
             os.Exit(1)
         }
 
-        project_map, err := get_projects(token)
+        projects, err := get_projects(token)
+        // project_map, err := get_projects(token)
         if err != nil {
             fmt.Fprintf(os.Stderr, toggl_err)
             os.Exit(1)
+        }
+
+        // Create a map of project IDs to project names
+        project_map := make(map[int64]string)
+        for _, project := range projects {
+            project_map[project.ID] = project.Name
         }
 
         // Print the entries to stdout, tab delimited
@@ -272,12 +426,8 @@ func main() {
     }
 }
 
-type TogglProject struct {
-    ID int64 `json:"id"`
-    Name string `json:"name"`
-}
 
-func get_projects(token string) (map[int64]string, error) {
+func get_projects(token string) ([]TogglProject, error) {
     // https://api.track.toggl.com/api/v9/me/projects
     req, err := http.NewRequest(http.MethodGet, "https://api.track.toggl.com/api/v9/me/projects", nil)
     if err != nil {
@@ -308,12 +458,39 @@ func get_projects(token string) (map[int64]string, error) {
         return nil, err
     }
 
-    // Create a map of project IDs to project names
-    projects := make(map[int64]string)
-    for _, project := range t {
-        projects[project.ID] = project.Name
+    return t, nil
+}
+
+func getWorkspaces(token string) ([]TogglWorkspace, error) {
+    // https://api.track.toggl.com/api/v9/me/workspaces
+    req, err := http.NewRequest(http.MethodGet, "https://api.track.toggl.com/api/v9/me/workspaces", nil)
+    if err != nil {
+        return nil, err
     }
-    return projects, nil
+
+    req.Header.Set("Content-Type", "application/json; charset=utf-8")
+    req.SetBasicAuth(token, "api_token")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return nil, err
+    }
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    // Parse as slice of TogglWorkspace
+    var t []TogglWorkspace
+    err = json.Unmarshal(body, &t)
+    if err != nil {
+        return nil, err
+    }
+
+    return t, nil
 }
 
 func daysInMonth(year int, month int) int {
