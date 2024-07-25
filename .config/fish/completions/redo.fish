89d63f3931a68a80150ac14d94b942c2a5aeff03
Mitchell Paulus <paulusm14@gmail.com>
2024-07-25 12:51:53 -0500

    Better redo completion

--- .config/fish/completions/redo.fish
+++ .config/fish/completions/redo.fish
@@ -1,1 +1,3 @@
-complete -c redo -a '(rt)'
+complete -c redo -f
+complete -c redo -k -a '(find  -name .git -prune -o -name .redo -prune -o -name __pycache__ -prune -o -type f "!" -name "*.do"  -print | cut -c 3- | sort)'
+complete -c redo -k -a '(rt)'
