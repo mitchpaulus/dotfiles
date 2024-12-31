652afa2838d7a98d39a335e4a59a0e535f458274
Mitchell Paulus <paulusm14@gmail.com>
2024-12-30 19:54:39 -0600

    Remove trailing whitespace in go files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1187,7 +1187,7 @@ if not in_windows then vim.api.nvim_create_autocmd('BufReadPost', { pattern = '*
 -- Setting the marks is required so that the cursor doesn't jump
 -- around.
 -- vim.cmd([[autocmd BufWrite * execute "normal! mz" |  keeppatterns %s/\v\s+$//e | normal `z]])
-vim.api.nvim_create_autocmd('BufWrite', { pattern = '*.cs,*.md,*.txt,*.lua,COMMIT_EDITMSG,*.msh', group = 'MPEvents', command = 'execute "normal! mz" |  keeppatterns %s/\\v\\s+$//e | normal `z' })
+vim.api.nvim_create_autocmd('BufWrite', { pattern = '*.cs,*.md,*.txt,*.lua,COMMIT_EDITMSG,*.msh,*.go', group = 'MPEvents', command = 'execute "normal! mz" |  keeppatterns %s/\\v\\s+$//e | normal `z' })
 
 function remove_trailing_blank_lines()
     -- Get number of lines in buffer. 0 is only for unloaded buffer.
