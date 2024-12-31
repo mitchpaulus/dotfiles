fd09fedf4f9f677f7cdb935ae97e519758e745c1
Mitchell Paulus <paulusm14@gmail.com>
2024-12-30 19:54:31 -0600

    Update msh completions

--- .config/fish/completions/msh.fish
+++ .config/fish/completions/msh.fish
@@ -1,4 +1,5 @@
 complete -c msh -s h -l help -d 'Show help'
 complete -c msh -l parse -d "Print the parsed Abstract Syntax Tree"
 complete -c msh -l lex -d "Print the tokens of the input"
+complete -c msh -l typecheck -d "Typecheck the input"
 complete -c msh -k -x -a "(__fish_complete_suffix .msh)"
