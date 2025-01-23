b5e9baf5ab075298f44eec8c9682b5fa12ec5fb4
Mitchell Paulus <paulusm14@gmail.com>
2025-01-23 16:27:24 -0600

    Add toggl completions

---
+++ .config/fish/completions/toggl.fish
@@ -0,0 +1,10 @@
+complete -c toggl -a current
+complete -c toggl -a stop
+complete -c toggl -a ts
+complete -c toggl -a ws
+complete -c toggl -a lunch
+complete -c toggl -a break
+complete -c toggl -a start
+complete -c toggl -a projects
+complete -c toggl -a newproj
+complete -c toggl -a update_proj
