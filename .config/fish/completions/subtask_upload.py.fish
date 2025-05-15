83eaabff5de295a520775278d32ec5618a59a3b1
Mitchell Paulus <paulusm14@gmail.com>
2025-05-15 16:29:52 -0500

    Fixes for 'sort' becoming a built in

--- .config/fish/completions/subtask_upload.py.fish
+++ .config/fish/completions/subtask_upload.py.fish
@@ -1,4 +1,5 @@
 complete -c 'subtask_upload.py' -s h -l help -d 'Show help message'
 complete -c 'subtask_upload.py' -l report
+complete -c 'subtask_upload.py' -l systems-manual
 complete -c 'subtask_upload.py' -d 'Project Id' -s p -x
 complete -c 'subtask_upload.py' -x -d "Project Drivers" -a "(subtask_upload.py drivers)"
