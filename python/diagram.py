7770e041a1c588cefd46b403497e50fdaff68072
Mitchell Paulus <paulusm14@gmail.com>
2025-07-05 21:30:34 -0500

    Add more capability to diagram.py

--- python/diagram.py
+++ python/diagram.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional, Union, Any
+from typing import Callable, Optional, Union
 import uuid
 import sys
 
@@ -11,9 +11,9 @@ import sys
 # Everything here has the positive y-axis going up
 
 
-Coord = float | Callable[[], float]
+Value = float | Callable[[], float]
 
-def compute(x):
+def compute(x) -> float:
     if callable(x):
         return x()
     else:
@@ -21,11 +21,13 @@ def compute(x):
 
 
 class Drawing:
-    def __init__(self) -> None:
+    def __init__(self, name = None) -> None:
         self._shapes = {}
         self._default_font_size = 12
         self._pp_slide_height = int(7.5 * 72) # 540 points
         self._pp_scale_factor = 1 # Scale factor of pts / input unit
+        self._default_stroke_width = 1
+        self._name = name if name else f"drawing{uuid.uuid4().hex}"
 
     def pp_scale_factor(self, scale_factor: float) -> 'Drawing':
         self._pp_scale_factor = scale_factor
@@ -39,6 +41,7 @@ class Drawing:
             raise KeyError(f"Shape with name {name} already exists")
 
         self._shapes[name] = Rectangle(name)
+        self._shapes[name]._drawing = self
         return self._shapes[name]
 
     def line(self, name: Optional[str] = None) -> 'Line':
@@ -49,6 +52,7 @@ class Drawing:
             raise KeyError(f"Shape with name {name} already exists")
 
         self._shapes[name] = Line(name)
+        self._shapes[name]._drawing = self
         return self._shapes[name]
 
     def text(self, name: Optional[str] = None, text: Optional[str] = None) -> 'Text':
@@ -75,10 +79,14 @@ class Drawing:
         self._default_font_size = font_size
         return self
 
+    def default_stroke_width(self, stroke_width: float) -> 'Drawing':
+        self._default_stroke_width = stroke_width
+        return self
+
     def to_svg(self, min_x: float, min_y: float, width: float, height: float):
         lines = []
 
-        lines.append(f'<svg viewBox="{min_x} {min_y} {width} {height}" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">\n')
+        lines.append(f'<svg viewBox="{min_x} {min_y} {width} {height}" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style="stroke-width: {self._default_stroke_width}">\n')
         for i in self._shapes:
             shape = self._shapes[i]
             lines.append(shape.to_svg())
@@ -110,33 +118,80 @@ class Drawing:
                 lines.append(shape.to_vba())
         return "".join(lines)
 
+    def merge(self, other: 'Drawing') -> 'Drawing':
+        """Merge another drawing into this one."""
+        for name, shape in other._shapes.items():
+            if name in self._shapes:
+                # Try with drawing name prefixed
+                name = f"{other._name}_{name}"
+            if name in self._shapes:
+                raise KeyError(f"Shape with name {name} already exists in drawing")
+
+            self._shapes[name] = shape
+        return self
+
+    def shift_x(self, dx: float) -> 'Drawing':
+        """Shift all shapes in the drawing by dx."""
+        for shape in self._shapes.values():
+            if hasattr(shape, 'shift_x'):
+                shape.shift_x(dx)
+            else:
+                raise NotImplementedError(f"Shape {shape} does not support shifting in x direction")
+        return self
+
+    def shift_y(self, dy: float) -> 'Drawing':
+        """Shift all shapes in the drawing by dy."""
+        for shape in self._shapes.values():
+            if hasattr(shape, 'shift_y'):
+                shape.shift_y(dy)
+            else:
+                raise NotImplementedError(f"Shape {shape} does not support shifting in y direction")
+        return self
 
 # Use fluent syntax
 class Rectangle:
     def __init__(self, name):
-        self._x: Coord = 0.0
-        self._y: Coord = 0.0
-        self._width: Coord = 1
-        self._height: Coord = 1
+        self._x: float = 0.0
+        self._y: float = 0.0
+        self._width: float = 1
+        self._height: float = 1
         self._name = name
         self._text = None
         self._font_size = None
         self._stroke_width = 1
+        self._drawing: Optional[Drawing] = None
 
         self._fill: Optional[tuple[int, int, int]] = None
 
-    def x(self, x = None) -> Any:
-        """Bottom left x-coordinate of the rectangle"""
-        if x is None:
-            return self._x
+    def dup(self, name: Optional[str] = None) -> 'Rectangle':
+        if name is None:
+            name = f"rect{uuid.uuid4().hex}"
 
+        if name in self._drawing._shapes:
+            raise KeyError(f"Shape with name {name} already exists in drawing")
+
+        new_rect = Rectangle(name)
+        new_rect._x = self._x
+        new_rect._y = self._y
+        new_rect._width = self._width
+        new_rect._height = self._height
+        new_rect._text = self._text
+        new_rect._font_size = self._font_size
+        new_rect._stroke_width = self._stroke_width
+        new_rect._fill = self._fill
+        new_rect._drawing = self._drawing
+        self._drawing._shapes[name] = new_rect
+
+        return new_rect
+
+
+    def x(self, x) -> 'Rectangle':
+        """Bottom left x-coordinate of the rectangle"""
         self._x = x
         return self
 
-    def y(self, y = None) -> Any:
+    def y(self, y = None) -> 'Rectangle':
         """Bottom left y-coordinate of the rectangle"""
-        if y is None:
-            return self._y
         self._y = y
         return self
 
@@ -153,10 +208,10 @@ class Rectangle:
         self._font_size = font_size
         return self
 
-    def x2(self) -> Coord:
+    def x2(self) -> Value:
         return lambda: compute(self._x) + compute(self._width)
 
-    def y2(self) -> Coord:
+    def y2(self) -> Value:
         return lambda: compute(self._y) + compute(self._height)
 
     def center_x(self):
@@ -165,11 +220,11 @@ class Rectangle:
     def center_y(self):
         return lambda:compute(self._y) + compute(self._height) / 2
 
-    def width(self, width: Coord) -> 'Rectangle':
+    def width(self, width: float) -> 'Rectangle':
         self._width = width
         return self
 
-    def height(self, height: Coord) -> 'Rectangle':
+    def height(self, height: float) -> 'Rectangle':
         self._height = height
         return self
 
@@ -192,6 +247,14 @@ class Rectangle:
 
     def right(self):
         return compute(self._x) + compute(self._width)
+
+    def shift_x(self, dx: float) -> 'Rectangle':
+        self._x = self._x + dx
+        return self
+
+    def shift_y(self, dy: float) -> 'Rectangle':
+        self._y = self._y + dy
+        return self
 
     def fill(self, r: int, g: int, b: int) -> 'Rectangle':
         # Validate the RGB values
@@ -258,24 +321,24 @@ class Rectangle:
 
         return rect_tag
 
-    def right_of(self, other, distance = 72) -> 'Rectangle':
-        self._x = lambda: compute(other._x) + compute(other._width) + distance
-        self._y = lambda: compute(other.center_y()) - compute(self._height) / 2
+    def right_of(self, other, distance = 0) -> 'Rectangle':
+        self._x = compute(other._x) + compute(other._width) + distance
+        self._y = compute(other.center_y()) - compute(self._height) / 2
         return self
 
     def below(self, other, distance = 72) -> 'Rectangle':
-        self._x = lambda: compute(other.center_x()) - compute(self._width) / 2
-        self._y = lambda: compute(other._y) + compute(other._height) + distance
+        self._x =  compute(other.center_x()) - compute(self._width) / 2
+        self._y =  compute(other._y) + compute(other._height) + distance
         return self
 
     def left_of(self, other, distance = 72) -> 'Rectangle':
-        self._x = lambda: compute(other._x) - compute(self._width) - distance
-        self._y = lambda: compute(other.center_y()) - compute(self._height) / 2
+        self._x =  compute(other._x) - compute(self._width) - distance
+        self._y =  compute(other.center_y()) - compute(self._height) / 2
         return self
 
     def above(self, other, distance = 72) -> 'Rectangle':
-        self._x = lambda: compute(other.center_x()) - compute(self._width) / 2
-        self._y = lambda: compute(other._y) - compute(self._height) - distance
+        self._x =  compute(other.center_x()) - compute(self._width) / 2
+        self._y =  compute(other._y) - compute(self._height) - distance
         return self
 
     def __str__(self):
@@ -283,26 +346,58 @@ class Rectangle:
 
 class Line:
     def __init__(self, name):
-        self._x1: Coord = 0.0
-        self._y1: Coord = 0.0
-        self._x2: Coord = 1
-        self._y2: Coord = 1
+        self._x1: Value = 0.0
+        self._y1: Value = 0.0
+        self._x2: Value = 1
+        self._y2: Value = 1
+        self._drawing: Optional[Drawing] = None
         self._name = name
+        self._dash_pattern: Optional[str] = None
 
-    def x1(self, x1: Coord) -> 'Line':
+    def x1(self, x1: Value) -> 'Line':
         self._x1 = x1
         return self
 
-    def y1(self, y1: Coord) -> 'Line':
+    def y1(self, y1: Value) -> 'Line':
         self._y1 = y1
         return self
 
-    def x2(self, x2: Coord) -> 'Line':
+    def x2(self, x2: Value) -> 'Line':
         self._x2 = x2
         return self
 
-    def y2(self, y2: Coord) -> 'Line':
+    def y2(self, y2: Value) -> 'Line':
         self._y2 = y2
+        return self
+
+    def coords(self, x1: Value, y1: Value, x2: Value, y2: Value) -> 'Line':
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        return self
+
+    def dup(self, name: Optional[str] = None) -> 'Line':
+        if name is None and self._drawing is not None:
+            name = f"line{len(self._drawing._shapes)}"
+        elif name is None and self._drawing is None:
+            name = f"line{uuid.uuid4().hex}"
+        if self._drawing is not None and name in self._drawing._shapes:
+            raise KeyError(f"Shape with name {name} already exists in drawing")
+
+        new_line = Line(name)
+        new_line._x1 = self._x1
+        new_line._y1 = self._y1
+        new_line._x2 = self._x2
+        new_line._y2 = self._y2
+        new_line._drawing = self._drawing
+        self._drawing._shapes[name] = new_line
+
+        return new_line
+
+    def dash_pattern(self, pattern: str) -> 'Line':
+        """Set the dash pattern for the line. Example: '5, 2' for a dashed line."""
+        self._dash_pattern = pattern
         return self
 
     def to_vba(self):
@@ -326,8 +421,32 @@ class Line:
         vba += f"line.Line.ForeColor.RGB = RGB(0, 0, 0)\n"
         return vba
 
+    def shift_x(self, dx: float) -> 'Line':
+        self._x1 = compute(self._x1) + dx
+        self._x2 = compute(self._x2) + dx
+        return self
+
+    def shift_y(self, dy: float) -> 'Line':
+        self._y1 = compute(self._y1) + dy
+        self._y2 = compute(self._y2) + dy
+        return self
+
+    def set_x(self, x: float) -> 'Line':
+        self._x1 = x
+        self._x2 = x
+        return self
+
+    def set_y(self, y: float) -> 'Line':
+        self._y1 = y
+        self._y2 = y
+        return self
+
     def to_svg(self):
-        return f'<line x1="{compute(self._x1)}" y1="{-compute(self._y1)}" x2="{compute(self._x2)}" y2="{-compute(self._y2)}" stroke="black" />\n'
+        element = f'<line x1="{compute(self._x1)}" y1="{-compute(self._y1)}" x2="{compute(self._x2)}" y2="{-compute(self._y2)}" stroke="black"'
+        if self._dash_pattern:
+            element += f' stroke-dasharray="{self._dash_pattern}"'
+        element += ' />\n'
+        return element
 
     def __str__(self):
         return f'Line({self.x1}, {self.y1}, {self.x2}, {self.y2})'
@@ -344,15 +463,15 @@ class Text:
         self._yAnchor = "auto"
         self._font_family = "sans-serif"
 
-    def x(self, x: Coord) -> 'Text':
+    def x(self, x: Value) -> 'Text':
         self._x = x
         return self
 
-    def y(self, y: Coord) -> 'Text':
+    def y(self, y: Value) -> 'Text':
         self._y = y
         return self
 
-    def text(self, text = None) -> Any:
+    def text(self, text = None) -> 'Text':
         if text is None:
             return self._text
 
@@ -422,15 +541,15 @@ class Circle:
         self._r = 10
         self._name = name
 
-    def x(self, x: Coord) -> 'Circle':
+    def x(self, x: Value) -> 'Circle':
         self._x = x
         return self
 
-    def y(self, y: Coord) -> 'Circle':
+    def y(self, y: Value) -> 'Circle':
         self._y = y
         return self
 
-    def r(self, r: Coord) -> 'Circle':
+    def r(self, r: Value) -> 'Circle':
         self._r = r
         return self
 
@@ -455,19 +574,19 @@ class Sensor:
 
         self._name = name
 
-    def x(self, x: Coord) -> 'Sensor':
+    def x(self, x: Value) -> 'Sensor':
         self._x = x
         return self
 
-    def y(self, y: Coord) -> 'Sensor':
+    def y(self, y: Value) -> 'Sensor':
         self._y = y
         return self
 
-    def width(self, width: Coord) -> 'Sensor':
+    def width(self, width: Value) -> 'Sensor':
         self._width = width
         return self
 
-    def height(self, height: Coord) -> 'Sensor':
+    def height(self, height: Value) -> 'Sensor':
         self._height = height
         return self
 
