4d2e01e114f9f16eafc438274d514dc1db18efc1
Mitchell Paulus <paulusm14@gmail.com>
2025-08-02 21:06:21 -0500

    Migrate eh from fish function to msh script

--- .config/fish/functions/eh.fish
+++
@@ -1,9 +0,0 @@
-function eh
-    if command -v explorer.exe >/dev/null 2>/dev/null
-        explorer.exe (wslpath -w (pwd))
-    else if command -v dolphin >/dev/null 2>/dev/null
-        dolphin . &
-    else
-        printf "No GUI file manager set up.\n"
-    end
-end
