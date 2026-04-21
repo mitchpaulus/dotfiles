8f3482c4d71d411be026dfc4cb8af1f64e57fd95
Mitchell Paulus <paulusm14@gmail.com>
2026-04-21 09:47:42 -0500

    Clean

--- go/todoist/todoist.go
+++ go/todoist/todoist.go
@@ -15,37 +15,6 @@ import (
 
 const workProjectID = "6Px2qFqGPQFg64Vw"
 
-// Example task object:
-/* { */
-/* "creator_id": "2671355", */
-/* "created_at": "2019-12-11T22:36:50.000000Z", */
-/* "assignee_id": "2671362", */
-/* "assigner_id": "2671355", */
-/* "comment_count": 10, */
-/* "is_completed": false, */
-/* "content": "Buy Milk", */
-/* "description": "", */
-/* "due": { */
-/* "date": "2016-09-01", */
-/* "is_recurring": false, */
-/* "datetime": "2016-09-01T12:00:00.000000Z", */
-/* "string": "tomorrow at 12", */
-/* "timezone": "Europe/Moscow" */
-/* }, */
-/* "duration": { */
-/* "amount": 15, */
-/* "unit": "minute" */
-/* }, */
-/* "id": "2995104339", */
-/* "labels": ["Food", "Shopping"], */
-/* "order": 1, */
-/* "priority": 1, */
-/* "project_id": "2203306141", */
-/* "section_id": "7025", */
-/* "parent_id": "2995104589", */
-/* "url": "https://todoist.com/showTask?id=2995104339" */
-/* } */
-
 type Task struct {
 	UserId         string   `json:"user_id"`
 	Id             string   `json:"id"`
