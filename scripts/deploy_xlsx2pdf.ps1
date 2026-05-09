7429ac391b7357aabc0c678d4649af81ca05353e
Mitchell Paulus <paulusm14@gmail.com>
2026-05-08 21:19:45 -0500

    Fix up some mshell

--- scripts/deploy_xlsx2pdf.ps1
+++ scripts/deploy_xlsx2pdf.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env mshell
-[wslpath -u -a 
-    [winenv LOCALAPPDATA] os; 
+[wslpath -u -a
+    [winenv LOCALAPPDATA] os;
 ] os; localappdata!
-[cp @DOTFILES "/scripts/xlsx2pdf.ps1" + @localappdata "/xlsx2pdf/xlsx2pdf.ps1" +];
+[cp $DOTFILES "/scripts/xlsx2pdf.ps1" + @localappdata "/xlsx2pdf/xlsx2pdf.ps1" +];
