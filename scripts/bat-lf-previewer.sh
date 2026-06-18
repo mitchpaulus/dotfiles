4ecc45f1295d8938a2ef12aa3a09ed129f9cde83
Mitchell Paulus <paulusm14@gmail.com>
2026-06-17 20:49:52 -0500

    Migrate to msh

--- scripts/bat-lf-previewer.sh
+++ scripts/bat-lf-previewer.sh
@@ -1,17 +1,10 @@
-#!/bin/sh
-
+#!/usr/bin/env mshell
 # See discussions:
 # https://github.com/gokcehan/lf/issues/234
 # https://github.com/sharkdp/bat/issues/490#issuecomment-511338924
 
 # $1 = file path
 # $2 = optional number of lines to show
-
-unset COLORTERM
-
-# if two arguments (program counts as one)
-if test "$#" -eq 3; then
-    bat --italic-text always --color always --style 'plain' --theme 'Visual Studio Dark+' --line-range ":$2" "$1"
-else
-    bat --italic-text always --color always --style 'plain' --theme 'Visual Studio Dark+' "$1"
-fi
+'COLORTERM' unsetenv
+args len 2 = if [--line-range $":{$2}"] else [] end lineRange!
+['bat' '--italic-text' always --color always --style 'plain' --theme 'Visual Studio Dark+' @lineRange $1]!
